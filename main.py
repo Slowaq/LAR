5366bcf83de1fe1a864dd6c1ea8b668ddd64b3ac
Jan Cejka <jancejka66@gmail.com>
2026-03-30 13:52:07 +0200

    Remove temporary code from main

--- main.py
+++ main.py
@@ -16,21 +16,5 @@ def main() -> None:
         sleep(0.1)
     return
 
-    # rozpoznani pylonu
-    filepath = os.path.join("sample_data", "recording_0001_mic_a_garaz_tma", "0010.mat")
-    data = scipy.io.loadmat(filepath)
-
-    rgb = data["image_rgb"]
-    hsv = rgb_to_hsv(rgb)
-
-    draw_hsv(hsv)
-
-    segments_image = find_ball_segments(hsv)
-
-    cv2.imshow("Ball Mask", segments_image)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()   
-
-
 if __name__ == "__main__":
     main()
